0f9fda5c31c0f22388fe08c37a36356f8574a61c
=abhishektanwar <abhishektanwar084@gmail.com>
2022-05-13 19:35:51 +0530

    added users model and api for user crud, follow, unfollow

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const express = require('express')
 
 const app = express();
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const helmet = require('helmet')
+const morgan = require('morgan')
+
+// routes
+const userRoute = require('./routes/users')
+const authRoute = require('./routes/auth')
+dotenv.config();
+
+// making conntection to mongo db
+mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true},()=>{
+  console.log("CONNTECTED TO MONGO DB")
+})
+
+// middlewares
+app.use(express.json()) // body parser for API requests 
+app.use(helmet())
+app.use(morgan("common"))
+
+app.use("/api/user",userRoute);
+app.use("/api/auth",authRoute);
 
 app.listen(8800,()=>{
   console.log("backend server is running")
 })
+
---
+++ routes/users.js
@@ -0,0 +1,110 @@
+const { route } = require("./auth");
+const bcrypt = require("bcrypt");
+const Users = require("../models/Users");
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  res.send("hey it is user route");
+});
+
+// update user
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    }
+    try{
+      const user = await Users.findByIdAndUpdate(req.body.userId,{$set:req.body});
+      res.status(200).json("Account has been updated")
+    }
+    catch(err){
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can update only your account!");
+  }
+});
+
+// delete user
+router.delete("/:id",async (req,res)=>{
+  if(req.body.userId === req.params.id || req.body.isAdmin){
+    try{
+      const user = await Users.findByIdAndDelete(req.params.id)
+      res.status(200).json("Account has been deleted");
+    }
+    catch(err){
+      console.log("err",err)
+      return res.status(500).json(err);
+    }
+  }
+  else{
+    return res.status(403).json("You can only delete your own account!");
+  }
+})
+
+// get a user
+router.get("/:id",async(req,res)=>{
+  try{
+    const user = await Users.findById(req.params.id)
+    const {password,updatedAt,...other} = user._doc
+    res.status(200).json(other)
+  }
+  catch(err){
+    res.status(500).json(err)
+  }
+})
+// follow a user
+router.put('/:idToFollow/follow',async(req,res)=>{
+  if(req.body.userId !== req.params.idToFollow){
+    try{
+      // current user is trying to follow user
+      const user = await Users.findById(req.params.idToFollow);
+      const currentUser = await Users.findById(req.body.userId);
+      if(!user.followers.includes(req.body.userId)){
+        // add current user to followers of user
+        await user.updateOne({$push:{followers:req.body.userId}})
+        // add user to following of current User
+        await currentUser.updateOne({$push:{following:req.params.idToFollow}})
+        res.status(200).json("User has been followed")
+      }else{
+        res.status(403).json("You already follow this user")
+      }
+    }
+    catch(err){
+      res.status(500).json(err)
+    }
+  }
+  else{
+    res.status(403).json("You cannot follow yourself")
+  }
+})
+
+// unfollow a user
+router.put("/:idToUnfollow/unfollow",async (req,res)=>{
+  if(req.body.userId !== req.params.idToUnfollow ){
+    try{
+      const user = await Users.findById(req.params.idToUnfollow)
+      const currentUser = await Users.findById(req.body.userId)
+      if(user.followers.includes(req.body.userId)){
+        await user.updateOne({$pull:{followers:req.body.iserId}})
+        await currentUser.updateOne({$pull:{following:req.params.idToUnfollow}})
+        res.status(200).json("user has been unfollowed")
+      }
+      else{
+        res.status(403).json("you do not follow this user")
+      }
+    }
+    catch(err){
+      res.status(500).json(err)
+    }
+  }
+  else{
+    res.status(403).json("You cannot unfollow yourself")
+  }
+})
+module.exports = router;
